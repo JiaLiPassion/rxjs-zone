8a4e3cf489982f812b88805ca09c237573df5a2f
JiaLi.Passion <JiaLi.Passion@gmail.com>
2017-08-09 04:44:59 +0900

    add zone rxjs sample

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,58 @@
-import { Component } from '@angular/core';
+//our root app component
+import { Component, NgModule, NgZone, VERSION } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
+import { Observable, Subscription, Subject } from 'rxjs/Rx';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <div>
+      <h2>Hello {{name}}</h2>
+    </div>
+    
+    <p>spaces$: {{ spaces$ | async }}</p>
+  `,
 })
 export class AppComponent {
-  title = 'app';
+  
+  public name:string;
+
+  public spaces$: Observable<any>;
+  
+  private store$ = new Subject();
+  private activitySubscription = new Subscription();
+  
+  constructor(private ngZone: NgZone) {
+    this.name = `Angular! v${VERSION.full}`
+  }
+  
+  ngOnInit() {
+    console.log('ngOninit');
+    
+    this.spaces$ = this.store$
+      .asObservable()
+      .do((value) => {
+        const inZone = NgZone.isInAngularZone();
+        console.log('spaces$ next => assertInAngularZone', value, inZone);
+      });
+                        
+    const repeater$ = Observable.of({})
+      .do(() => {
+        const num = Math.floor(Math.random() * 100);
+        console.log('Poll action dispatched', num);
+        this.store$.next('store$.next(' + num + ')');
+      })
+      .delay(2000)
+      .repeat();
+      
+    this.ngZone.runOutsideAngular(() => {
+      this.activitySubscription = repeater$.subscribe(
+        () => { },
+        (error) => {
+          console.error('refresh error', error);
+        },
+        () => console.info('finished')
+      );
+   });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import 'zone.js/dist/zone-patch-rxjs';
 
 import { AppComponent } from './app.component';
 
